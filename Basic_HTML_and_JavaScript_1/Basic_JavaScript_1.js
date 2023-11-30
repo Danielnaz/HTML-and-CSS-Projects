cb14aa412e147dc926c193b9ecac55d1597d775b
Danielnaz <150403920+Danielnaz@users.noreply.github.com>
2023-11-29 19:24:35 -0500

    pushing html and JavaScript files

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+<script>
+alert("Hello, World!");
+    function My_First_Function() {
+	var string = "Kiss me, Im Irish!";
+	var result = string.fontcolor("green");
+	document.getElementById("Irish").innerHTML =
+	result;
+}
+</script>
