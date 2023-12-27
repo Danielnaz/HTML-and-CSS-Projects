bf89621d1abf34b3fc9bb65df42a155683c283d9
Danielnaz <150403920+Danielnaz@users.noreply.github.com>
2023-12-26 19:50:46 -0500

    Updates

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++
@@ -1,9 +0,0 @@
-<script>
-alert("Hello, World!");
-    function My_First_Function() {
-	var string = "Kiss me, Im Irish!";
-	var result = string.fontcolor("green");
-	document.getElementById("Irish").innerHTML =
-	result;
-}
-</script>
